ffebfd740120bb529113f61cc0e634d6ccc3eab7
Artem <artem.kh4rchenko@ya.ru>
2022-08-04 14:18:50 +0700

    создал слайдер с ограничителями

--- GoalsAppSwiftUI/ContentView.swift
+++ GoalsAppSwiftUI/ContentView.swift
@@ -21,6 +21,14 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 struct Home: View {
+    
+    @State var maxHeight: CGFloat = UIScreen.main.bounds.height / 3
+    
+    //Slider cвойства
+    @State var sliderProgress: CGFloat = 0
+    @State var sliderHeight: CGFloat = 0
+    @State var lastDragValue: CGFloat = 0
+    
     var body: some View {
         
         NavigationView {
@@ -29,7 +37,41 @@ struct Home: View {
                 
                 //Slider
                 
-                Text("Hello")
+                ZStack(alignment: .bottom) {
+                    
+                    Rectangle()
+                        .fill(Color("slider").opacity(0.15))
+                    
+                    Rectangle()
+                        .fill(Color("slider"))
+                        .frame(height: sliderHeight)
+                }
+                .frame(width: 100, height: maxHeight)
+                .cornerRadius(20)
+                .gesture(DragGesture(minimumDistance: 0)
+                    .onChanged({ value in
+                        //получаем drag значение
+                        let translation = value.translation
+                        sliderHeight = -translation.height + lastDragValue
+                        
+                        // ограничиваем высоту слайдера
+                        sliderHeight = sliderHeight > maxHeight ? maxHeight : sliderHeight
+                        
+                        //негативное ограничение
+                        sliderHeight = sliderHeight >= 0 ? sliderHeight : 0
+                    
+                        
+                    }).onEnded({ value in
+                        // ограничиваем высоту слайдера
+                        sliderHeight = sliderHeight > maxHeight ? maxHeight : sliderHeight
+                        
+                        //негативное ограничение
+                        sliderHeight = sliderHeight >= 0 ? sliderHeight : 0
+                        
+                        //cохраняем последнее значение
+                        lastDragValue = sliderHeight
+                    })
+                )
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(Color("bg").ignoresSafeArea())
